41974b045ee02d7592e0f7b6d4fffc8af535d268
Lisa Ugray <lisa.ugray@shopify.com>
2020-04-14 11:12:21 -0400

    Better poll

--- nix/poll.nix
+++ nix/poll.nix
@@ -1,21 +1,12 @@
 let
   pkgs = import <nixpkgs> {};
 in
-  pkgs.runCommand "poll" {} ''
-    mkdir -p $out/bin
-    cat << EOF >> $out/bin/poll
-    #! ${pkgs.stdenv.shell}
-    echo -en '\e[?1049h' # Enable alternative screen buffer
-    echo -en '\e[2J\e[H' # Clear the screen and go to top left
-    while true; do
-      "\$@"
+  pkgs.writeShellScriptBin "poll" ''
+    trap "echo -en '\e[?1049l'" EXIT
+    echo -en '\e[?1049h'
+    until [ "$input" = "q" ]; do
+      echo -en '\e[H'
+      "$@"
       read -rs -n1 -t1 input
-      if [ "\$input" = "q" ]; then
-        break
-      fi
-      echo -en '\e[H' # Go to top left
     done
-    echo -en '\e[?1049l' # Disable alternative screen buffer
-    EOF
-    ${pkgs.coreutils}/bin/chmod +x $out/bin/poll
   ''
