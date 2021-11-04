6566e118a3974b19763a8147406ddc350e6e7cd5
Lisa Ugray <lisa.ugray@shopify.com>
2021-11-04 14:14:42 -0400

    Back

--- .shellfuncs/back.sh
+++ .shellfuncs/back.sh
@@ -1,6 +1,7 @@
 function back {
-	if [[ $1 =~ ^[1-9][0-9]*$ ]]; then
-		path=`yes ".." | head -n $1 | tr '\n' '/'`
-		cd $path
-	fi
+  COUNT="$1"
+  if ! [[ $COUNT =~ ^[1-9][0-9]*$ ]]; then
+    COUNT=1
+  fi
+  cd $(yes ".." | head -n $COUNT | tr '\n' '/')
 }
